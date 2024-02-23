5f701681891fa5ee7e85c21688027cbe374b1e56
Gnarizard <toefoo@protonmail.com>
2024-02-23 04:51:23 -0800

    After engaging with GPT to gain insights and understanding of complex code

--- gpt/RyanA_gpt/RyanA_complicatedCode.py
+++ gpt/RyanA_gpt/RyanA_complicatedCode.py
@@ -1,13 +1,18 @@
-# Before engaging with GPT
+# After engaging with GPT
 
-def fibonacci(n, memo={}):
-    if n in memo:
-        return memo[n]
-    if n <= 2:
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    if n == 1:
         return 1
-    else:
-        memo[n] = fibonacci(n-1, memo) + fibonacci(n-2, memo)
-        return memo[n]
+
+    fib = [0] * (n + 1)
+    fib[1] = 1
+
+    for i in range(2, n + 1):
+        fib[i] = fib[i - 1] + fib[i - 2]
+
+    return fib[n]
 
 # Example usage
 print(fibonacci(10))  # Should return 55

2699661bed2c96d23eec24c39cd68d6b3015b6e9
SibeW <143662887+SibeW@users.noreply.github.com>
2024-02-23 11:14:47 -0800

    Add comments explaining what certain syntax does
    I added comments explaining what certain syntax does for future developers who may also struggle to understand what Serialize Field and Tooltip do.

--- gpt/AnthonyV_gpt/lego/OneButtonMinifigController.cs
+++ gpt/AnthonyV_gpt/lego/OneButtonMinifigController.cs
@@ -16,6 +16,23 @@ namespace Unity.LEGO.Minifig
             Distance
         }
 
+        /* Syntax: Serialize Field
+         * 
+         * This attribute tells Unity to serialize the field, 
+         * meaning it should be shown in the Unity editor 
+         * and its value should be saved when the scene is saved. 
+         * Serialized fields can be accessed and modified in the Unity editor, 
+         * allowing developers to configure them without changing the code.
+         */
+        /* Syntax:Tooltip
+         * 
+         * This part of the attribute provides a tooltip 
+         * that appears when hovering over the field in the Unity editor. 
+         * It's a descriptive text that helps developers understand the purpose 
+         * of the field without needing to reference additional documentation. 
+         * In this case, it clarifies that the field represents 
+         * the movement type that will be activated when the scene starts.
+         */
         [SerializeField, Tooltip("The movement type to activate on start.")]
         StartMovementType startMovementType = StartMovementType.No;
 

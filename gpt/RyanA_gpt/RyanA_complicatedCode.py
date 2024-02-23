0adee5cb86b2182642310e1424ee584fc27e7a36
Gnarizard <toefoo@protonmail.com>
2024-02-23 04:32:32 -0800

    pushing complicated code segment to create an issue

---
+++ gpt/RyanA_gpt/RyanA_complicatedCode.py
@@ -0,0 +1,11 @@
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    else:
+        memo[n] = fibonacci(n-1, memo) + fibonacci(n-2, memo)
+        return memo[n]
+
+# Example usage
+print(fibonacci(10))  # Should return 55

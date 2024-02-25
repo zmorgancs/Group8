914ce4107f2929532688eae611bb83bfb5fb759c
Umbriel <ajbunnell1@gmail.com>
2024-02-24 20:08:23 -0800

    Add the complex code implementations

--- gpt/AdamB_gpt/PixelPainter.cs
+++ gpt/AdamB_gpt/PixelPainter.cs
@@ -9,35 +9,55 @@ using System.Collections.Generic;
 
 namespace Incursion.Drawing
 {
-    public class PixelPainter /// Make static?
+    public class PixelPainter
     {
+        // Access the map instance from Main
         private Map map => Main.Instance.Map;
-        public void Load() {
-            PaintArray = new Color[map.PaintLayer.Width * map.PaintLayer.Height];
-        }
+
+        // Array to store colors for painting territories
         private Color[] PaintArray;
+
+        // Initialize the PaintArray based on map dimensions
+        public void Load()
+        {
+            int arraySize = map.PaintLayer.Width * map.PaintLayer.Height;
+            PaintArray = new Color[arraySize];
+        }
+
+        // Convert territories to a format suitable for painting on the map
         public void ConvertTerritoriesToPaint()
         {
-            if(map.territoryUpdateQueue.Count <= 0)
+            // Check if there are any pending territory updates
+            if (map.territoryUpdateQueue.Count <= 0)
             {
                 return;
             }
-            foreach (Point coords in map.territoryUpdateQueue)
+
+            // Iterate through each coordinate in the update queue
+            foreach (Point coords in new List<Point>(map.territoryUpdateQueue))
             {
                 int x = coords.X;
                 int y = coords.Y;
+
+                // Update the territory and get the owner's color
                 if (!map.territory[x, y].Update())
                 {
-                    int i = x + y * map.width;
-                    Color color = Main.Instance.players[map.territory[x, y].Owner].playerColor;
-                    PaintArray[i] = color;
+                    int arrayIndex = x + y * map.width;
+                    Color territoryColor = Main.Instance.players[map.territory[x, y].Owner].playerColor;
+                    PaintArray[arrayIndex] = territoryColor;
+
+                    // Remove the coordinate from the update queue
                     map.territoryUpdateQueue.Remove(coords);
                 }
             }
+
+            // Reset the update queue if it's empty
             if (map.territoryUpdateQueue.Count <= 0)
             {
                 map.territoryUpdateQueue = new HashSet<Point>();
             }
+
+            // Update the map's paint layer with the new data
             map.PaintLayer.SetData(PaintArray);
         }
     }

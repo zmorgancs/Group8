258e36e4320a8c19b5cb7267c15183c4031536f5
Snapshot0010 <snapshot00010@gmail.com>
2024-02-25 13:24:36 -0800

    Adding in the additional documentation as suggested by ChatGPT

--- gpt/CamdenY_gpt/KNN.py
+++ gpt/CamdenY_gpt/KNN.py
@@ -1,37 +1,62 @@
 def KNN(kVal):
+    """
+    Implement the k-Nearest Neighbors (KNN) algorithm for classification.
+
+    Parameters:
+    - kVal (int): The number of nearest neighbors to consider for classification.
+
+    Reads data from 'MNIST_training.csv' as training data and 'MNIST_test.csv' as test data.
+    Calculates the accuracy of the algorithm and prints the result.
+
+    Returns:
+    None
+    """
     trainingData = []
     testData = []
     correctPredict = 0
     predictedLabels = []
 
-    with open('MNIST_training.csv','r') as trainingFile:
+    # Read training data from 'MNIST_training.csv'
+    with open('MNIST_training.csv', 'r') as trainingFile:
         reader = csv.reader(trainingFile)
-        next(reader)
+        next(reader)  # Skip header
         for line in reader:
             if line:
-                    trainingData.append(line)
+                trainingData.append(line)
 
-    with open('MNIST_test.csv','r') as testFile:
+    # Read test data from 'MNIST_test.csv'
+    with open('MNIST_test.csv', 'r') as testFile:
         reader = csv.reader(testFile)
-        next(reader)
+        next(reader)  # Skip header
         for line in reader:
             if line:
                 testData.append(line)
 
+    # Classify each point in the test data
     for i in testData:
         closestLabels = []
-        pointDistance = np.array(euclDistance(i,trainingData))
+
+        # Calculate Euclidean distances between the test point and all training points
+        pointDistance = np.array(euclDistance(i, trainingData))
         trData = np.array(trainingData)
+
+        # Sort training points based on distances
         sortedData = pointDistance.argsort()
         closestPoints = trData[sortedData]
-        for k in itertools.islice(closestPoints,kVal):
+
+        # Select k nearest neighbors
+        for k in itertools.islice(closestPoints, kVal):
             closestLabels.append(k[0])
-        predictedLabel = max(set(closestLabels),key=closestLabels.count)
+
+        # Predict the label based on majority vote
+        predictedLabel = max(set(closestLabels), key=closestLabels.count)
         predictedLabels.append(predictedLabel)
 
-    for (i,j) in zip(testData,predictedLabels):
-        if(i[0] == j):
-             correctPredict += 1
+    # Evaluate the accuracy of the predictions
+    for (i, j) in zip(testData, predictedLabels):
+        if i[0] == j:
+            correctPredict += 1
 
-    perecentCorrect = float(correctPredict/len(testData)) * 100
-    print(" The percent correct with a k value of " + str(kVal) + " is " + str(perecentCorrect) + "%")
+    # Print the accuracy percentage without changing the format
+    percentCorrect = float(correctPredict / len(testData)) * 100
+    print(" The percent correct with a k value of " + str(kVal) + " is " + str(percentCorrect) + "%")

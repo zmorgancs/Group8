d5d8cdcceef19d642c2902f0b52493149424c450
Snapshot0010 <snapshot00010@gmail.com>
2024-02-25 10:02:41 -0800

    Adding code for gpt lab

---
+++ gpt/CamdenY_gpt/EuclDistance.py
@@ -0,0 +1,11 @@
+import itertools
+import math
+
+def euclDistance(dataPoint, trData):
+    distance = []
+    for i in trData:
+        currDistance = 0.0
+        for (j,k) in zip(itertools.islice(dataPoint,1,None),itertools.islice(i,1,None)):
+            currDistance += (float(j) - float(k)) ** 2
+        distance.append(math.sqrt(currDistance))
+    return distance

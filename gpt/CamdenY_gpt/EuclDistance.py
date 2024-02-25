8c727099454595327638b88648834e83074800de
Snapshot0010 <snapshot00010@gmail.com>
2024-02-25 12:36:54 -0800

    Implementing the refactoring suggestions from ChatGPT

--- gpt/CamdenY_gpt/EuclDistance.py
+++ gpt/CamdenY_gpt/EuclDistance.py
@@ -1,11 +1,8 @@
 import math
-import itertools
 
-def euclDistance(dataPoint, trData):
-    distance = []
-    for i in trData:
-        currDistance = 0.0
-        for (j,k) in zip(itertools.islice(dataPoint,1,None),itertools.islice(i,1,None)):
-            currDistance += (float(j) - float(k)) ** 2
-        distance.append(math.sqrt(currDistance))
-    return distance
+def euclidean_distance(data_point, training_data):
+    distances = []
+    for training_point in training_data:
+        curr_distance = sum((float(j) - float(k)) ** 2 for j, k in zip(data_point[1:], training_point[1:]))
+        distances.append(math.sqrt(curr_distance))
+    return distances
